02b4f6c76bfe2c7706d3ad1b9090460b3ae46579
Grandpa-Newton <ivan.gromyko.2804@gmail.com>
2023-03-12 19:13:49 +0300

    Добавлены классы Baloon, BattleGame и библиотека классов PrizesLibrary

---
+++ GameLibrary/Balloon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class Balloon
+    {
+        public int Armour { get; set; } = 0;
+        public int Health { get; set; } = 100;
+        public bool CheckAlive(int health, int armour)
+        {
+            if (health <= 0)
+                return false;
+            else
+                return true;
+             
+        }
+    }
+}
